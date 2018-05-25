57daf06e69c22e50858fe6e0bd793991c4a62968
mariane giambastiani <mgiambastiani@cipucrs.pucrs.br>
2018-05-25 15:30:54 -0300

    configuracao jdk e criacao de teste

--- java/src/main/java/exemplo/java/App.java
+++ java/src/main/java/exemplo/java/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( getGreeting() );
     }
+
+	public static String getGreeting() {
+		return "Hello, World!";
+	}
 }
